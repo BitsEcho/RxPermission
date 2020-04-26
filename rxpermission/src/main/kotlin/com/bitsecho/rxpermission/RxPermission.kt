4e66cb39cf244a651b6974728e8e2a20f0dd06df
BitsEcho <bitsecho@163.com>
2020-04-26 11:46:57 +0800

    Init

---
+++ rxpermission/src/main/kotlin/com/bitsecho/rxpermission/RxPermission.kt
@@ -0,0 +1,73 @@
+package com.bitsecho.rxpermission
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import com.bitsecho.util.RxBus
+import com.bitsecho.rxpermission.RxPermissionFragment.Companion.TAG
+import io.reactivex.rxjava3.disposables.Disposable
+
+fun FragmentActivity.requestRxPermission(vararg permissions: String): Observable<PermissionResult> = RxPermission(this).request(*permissions)
+fun FragmentActivity.requestPermissionOnce(vararg permissions: String, subscribe: (result: PermissionResult)->Unit) {
+    var disposable: Disposable? = null
+    disposable = requestRxPermission(*permissions).subscribe {
+        subscribe(it)
+        disposable?.dispose()
+    }
+}
+
+class RxPermission(private val activity: FragmentActivity) {
+    fun request(vararg permissions: String): Observable<PermissionResult> {
+        val rxPermissionFragment = getRxPermissionFragment()
+        val permissionsResultObs = rxPermissionFragment.permissionObs
+        rxPermissionFragment.requestRxPermissions(permissions)
+        return permissionsResultObs
+    }
+
+    private fun getRxPermissionFragment(): RxPermissionFragment {
+        val fragmentManager = activity.supportFragmentManager
+        val rxPermissionFragment = fragmentManager.findFragmentByTag(TAG)
+        return if(rxPermissionFragment == null) {
+            val rxPermissionsFragment = RxPermissionFragment()
+            fragmentManager.beginTransaction()
+                .add(rxPermissionsFragment, TAG)
+                .commitAllowingStateLoss()
+            fragmentManager.executePendingTransactions()
+            rxPermissionsFragment
+        } else {
+            rxPermissionFragment as RxPermissionFragment
+        }
+    }
+}
+
+data class PermissionResult(val isGrant: Boolean, val shouldShowRequestPermissionRationale: Boolean)
+
+class RxPermissionFragment: Fragment() {
+    companion object {
+        const val PERMISSIONS_REQUEST_CODE = 1125
+        const val TAG = "RxPermissionFragment"
+    }
+
+    private val permissionsBus = RxBus<PermissionResult>()
+    val permissionObs: Observable<PermissionResult> get() = permissionsBus.obs.observeOn(AndroidSchedulers.mainThread())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+    }
+
+    fun requestRxPermissions(permissions: Array<out String>) = requestPermissions(permissions, PERMISSIONS_REQUEST_CODE)
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode != PERMISSIONS_REQUEST_CODE) return
+        val shouldShowRequestPermissionRationaleResult = permissions.map { shouldShowRequestPermissionRationale(it) }
+            .reduce { acc: Boolean, b: Boolean -> acc or b }
+        val grantResult = grantResults.map { it == PackageManager.PERMISSION_GRANTED }
+            .reduce { acc: Boolean, b: Boolean -> acc and b }
+        permissionsBus.post(PermissionResult(grantResult, shouldShowRequestPermissionRationaleResult))
+    }
+}
---
+++ sample/src/main/kotlin/com/bitsecho/rxpermission/activity/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.bitsecho.rxpermission.activity
+
+import android.Manifest
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bitsecho.rxpermission.R
+import com.bitsecho.rxpermission.requestPermissionOnce
+import com.bitsecho.rxpermission.requestRxPermission
+import io.reactivex.rxjava3.disposables.Disposable
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        findViewById<Button>(R.id.camera).setOnClickListener {
+            var dis: Disposable? = null
+            //Subscribe permission result by Observable. Manage Observable manually.
+            dis = requestRxPermission(Manifest.permission.CAMERA, Manifest.permission.VIBRATE).subscribe {
+                    val grant = if (it.isGrant) "Camera and Vibrate Grant" else "Camera or Vibrate Grant"
+                    Toast.makeText(this, grant, Toast.LENGTH_SHORT).show()
+                    dis?.dispose()
+                }
+        }
+
+        findViewById<Button>(R.id.read_storage).setOnClickListener {
+            //Trigger permission result once by code block. Briefer usage.
+            requestPermissionOnce(Manifest.permission.READ_EXTERNAL_STORAGE) {
+                    val grant = if (it.isGrant) "Read Storage Grant" else "Read Storage NOT Grant"
+                    Toast.makeText(this, grant, Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+}
